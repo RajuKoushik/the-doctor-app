dc5349997ab802a60ef56f291841b156f5a01129
rajukoushik <g.rajukoushik@gmail.com>
2017-12-08 23:07:50 +0530

    Added models.py

--- api/models.py
+++ api/models.py
@@ -1,6 +1,37 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
+from datetime import datetime
 from django.db import models
+from rest_framework.authtoken.models import Token
+from django.conf import settings
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.utils import timezone
 
-# Create your models here.
+
+class PatientInfo(models.Model):
+    def __str__(self):
+        return self.user.username
+
+    patient_unique_name = models.CharField(max_length=5000)
+    age = models.PositiveIntegerField(default=0)
+    details = models.CharField(max_length=5000)
+    join_date = models.DateTimeField('Join published', default='2011-11-11 11:11')
+
+
+class Doctor(models.Model):
+    def __str__(self):
+        return self.user.username
+
+    doctor_age = models.CharField(max_length=5000)
+    doctor_age = models.PositiveIntegerField(default=0)
+    doctor_details = models.CharField(max_length=5000)
+    patient_name = models.IntegerField()
+
+
+class Pharmacist(models.Model):
+    def __str__(self):
+        return self.user.username
+
+    pharmacist_name = models.CharField(max_length=5000)
+    pharmacist_age = models.PositiveIntegerField(default=0)
+    pharmacist_details = models.CharField(max_length=5000)
+    patient_name = models.IntegerField()
