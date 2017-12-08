f20a028900567f539a2b6041ed11f7142427c1a4
rajukoushik <g.rajukoushik@gmail.com>
2017-12-09 01:33:01 +0530

    Updated registration module

--- api/models.py
+++ api/models.py
@@ -11,27 +11,30 @@ class PatientInfo(models.Model):
     def __str__(self):
         return self.user.username
 
-    patient_unique_name = models.CharField(max_length=5000,null=True)
+    patient_unique_name = models.CharField(max_length=5000, null=True)
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     age = models.PositiveIntegerField(default=0)
-    details = models.CharField(max_length=5000,null=True)
+    details = models.CharField(max_length=5000, null=True)
+
 
 
 class Doctor(models.Model):
     def __str__(self):
         return self.user.username
 
-    doctor_age = models.CharField(max_length=5000)
-    doctor_age = models.PositiveIntegerField(default=0)
-    doctor_details = models.CharField(max_length=5000)
-    patient_name = models.IntegerField()
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,default=1)
+    doctor_name = models.CharField(max_length=5000,null=True)
+    doctor_details = models.CharField(max_length=5001)
+    doctor_tame = models.CharField(max_length=5000, null=True)
+    patient = models.ForeignKey(PatientInfo,null=True)
 
 
 class Pharmacist(models.Model):
     def __str__(self):
         return self.user.username
 
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,default=1)
     pharmacist_name = models.CharField(max_length=5000)
     pharmacist_age = models.PositiveIntegerField(default=0)
     pharmacist_details = models.CharField(max_length=5000)
-    patient_name = models.IntegerField()
+    patient = models.ForeignKey(PatientInfo,null=True)
--- api/views.py
+++ api/views.py
@@ -15,15 +15,28 @@ def signup(request):
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
+            first_name = form.cleaned_data.get('first_name')
+            last_name = form.cleaned_data.get('last_name')
 
             user = authenticate(username=username, password=raw_password)
             if form.cleaned_data.get('select') == 'Patient':
-                patientinfo = models.PatientInfo(user=user)
+                patientinfo = models.PatientInfo(user=user, patient_unique_name=username,
+                                                 details=str(first_name + ' additional_info ' + last_name))
+
                 patientinfo.save()
+            elif form.cleaned_data.get('select') == 'Doctor':
+                doctorinfo = models.Doctor(user=user, doctor_name=username,
+                                           doctor_details=str(first_name + ' additional_info ' + last_name))
+
+                doctorinfo.save()
+            elif form.cleaned_data.get('select') == 'Pharmacist':
+                pharmacistinfo = models.Doctor(user=user, pharmacist_name=username,
+                                               pharmacist_details=str(first_name + ' additional_info ' + last_name))
+
+                pharmacistinfo.save()
             login(request, user)
 
             return redirect('home')
-
 
     else:
         form = SignUpForm()
